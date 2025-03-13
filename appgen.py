7ff51f467bf2ab9175d793ab9d2c3ce161547d2c
hamdani2020 <hamdanialhassangandi2020@gmail.com>
2025-03-13 12:32:36 +0000

    Gemini 2.0

--- appgen.py
+++ appgen.py
@@ -36,7 +36,7 @@ if "parent_message_id" not in st.session_state:
 # Load YOLO model
 @st.cache_resource
 def load_yolo_model():
-    return YOLO("model.pt")
+    return YOLO("model.pt")  # You can change to a different pre-trained model if needed
 
 
 # Perform object detection
@@ -99,8 +99,8 @@ def send_amaliai_request(
     api_key,
     prompt,
     image_base64=None,
-    model_name="gemini-1.5-flash-latest",
-    stream=False,
+    model_name="gemini-2.0-flash",
+    stream=True,
 ):
     """
     Function to send request to AmaliAI.
@@ -205,7 +205,6 @@ def main():
     st.set_page_config(
         page_title="🍅🍆GreenAI",
         page_icon="🌽",
-        layout="wide",
         initial_sidebar_state="expanded",
     )
 
@@ -251,7 +250,9 @@ def main():
     # File uploader for images
     uploaded_file = st.file_uploader("AgroDetect", type=["jpg", "jpeg", "png"])
     st.markdown("---")
-    st.markdown("*Developed with 💚 for Agricultural Innovation*")
+    st.markdown(
+        "*Developed with 💚 for Agricultural Innovation and Happy Farmer's Day Celebration*"
+    )
 
     if uploaded_file is not None:
         # Perform object detection
@@ -359,3 +360,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
